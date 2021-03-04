9ae43a02ee2fd82d8d3e97e35a1e75a3270d1ed4
donsilva-dev <douglas.n.silva30@gmail.com>
2021-03-04 19:41:23 -0300

    Finalizando meu projeto express.

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.send('Hello world')
+const listaUsuarios = [ 
+    
+        'Ivens',
+        'Jaison',
+        'Maria',
+        'Natalia',
+        'Ariel',
+        'Nykolle'
+]
+    
+
+// Query Params
+// Ex: Localhost:3000/usuarios?id=1
+
+app.get('/usuarios', (req, res) => {
+    const {nome} =  req.query
+    let listaDeRetorno = listaUsuarios.filter(i => i.includes(nome || ''))
+    return res.json(listaDeRetorno)
+});
+
+
+// Route Params
+// Ex: ecommer.com.br/produtos/4/detalhes
+
+app.get('/:usuario', (req, res) => {
+    const { usuario } = req.params;
+    
+    return res.send(`Bem vindo! ${usuario}`)
 })
 
 app.listen(3000, () => {
